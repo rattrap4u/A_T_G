07eae3466b6d35248a139b8f37d7a4f376ae271f
rattrap4u <ratrap4u@gmail.com>
2024-04-01 18:58:51 +0530

    terraform and yaml code fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,7 @@ resource "azurerm_resource_group" "rg" {
   location = "centralindia"
 }
 
+
 resource "azurerm_storage_account" "STA" {
   name                     = "rgportstorage"
   resource_group_name      = azurerm_resource_group.rg.name
