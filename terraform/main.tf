b74433a56a270035bb73fdf65b5887992a064d0c
rattrap4u <ratrap4u@gmail.com>
2024-04-03 11:23:10 +0530

    terraform config file fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_storage_account" "STA" {
+resource "azurerm_storage_account" "rgportstorage" {
   name                     = var.sname
   resource_group_name      = var.rgname
   location                 = var.location
