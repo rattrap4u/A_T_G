034d64cde4c844abbca105838da77bf93b92934b
rattrap4u <ratrap4u@gmail.com>
2024-04-03 11:18:35 +0530

    terraform config file fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,10 +9,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id   = var.subscription_id
-  tenant_id         = var.tenant_id
-  client_id         = var.client_id
-  client_secret     = var.client_secret
 }
 
 resource "azurerm_resource_group" "rg" {
