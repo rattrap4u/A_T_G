1e0f7830e85612e17cac9fadeb96fc6c905e3820
rattrap4u <ratrap4u@gmail.com>
2024-04-03 11:17:08 +0530

    terraform config file fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,3 @@
-variable "subscription_id" {
-  description = "Azure subscription ID"
-}
-
-variable "tenant_id" {
-  description = "Azure tenant ID"
-}
-
-variable "client_id" {
-  description = "Azure client ID"
-}
-
-variable "client_secret" {
-  description = "Azure client secret"
-}
-
 variable "rgname" {
   description = "Resource Group Name"
   default     = "rg"
