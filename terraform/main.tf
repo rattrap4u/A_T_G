593a954c0e0a8cec5d25bdc709fa4a504eda1ae6
rattrap4u <ratrap4u@gmail.com>
2024-04-03 15:00:20 +0530

    terraform config file fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,27 +2,30 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.7.0"
+      version = "=3.0.0"
     }
-  }
-
-  # Update this block with the location of your terraform state file
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-github-actions-state"
-    storage_account_name = "terraformgithubactions"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    
   }
 }
 
 provider "azurerm" {
   features {}
-  use_oidc = true
+  skip_provider_registration = true # Skip automatic provider registration
+
 }
 
-# Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
-resource "azurerm_resource_group" "rg-aks" {
-  name     = var.resource_group_name
+resource "azurerm_resource_group" "rg" {
+  name     = var.rgname
   location = var.location
+}
+
+resource "azurerm_storage_account" "rg" {
+  name                     = "ptsta"
+  resource_group_name      = "rg"
+  location                 = "centralindia"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "dev"
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,11 @@
 variable "rgname" {
-    description = "Resource Group Name"
-    default     = "rg"
-    type        = string
+  description = "Resource Group Name"
+  default     = "rg"
+  type        = string
 }
 
 variable "location" {
-    description = "Azure location"
-    default     = "centralindia"
-    type        = string
-}
-
-variable "sname" {
-    description = "Azure Storage Account"
-    default     = "rgportstorage"
-    type        = string
+  description = "Azure location"
+  default     = "centralindia"
+  type        = string
 }
