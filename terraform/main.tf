e851b95c054deb696f0d07d49036a06181b2db56
rattrap4u <ratrap4u@gmail.com>
2024-04-03 14:09:00 +0530

    terraform config file fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,30 +2,27 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = ">= 3.7.0"
     }
+  }
+
+  # Update this block with the location of your terraform state file
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-github-actions-state"
+    storage_account_name = "terraformgithubactions"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
   }
 }
 
 provider "azurerm" {
   features {}
-  skip_provider_registration = true  # Skip automatic provider registration
-
+  use_oidc = true
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.rgname
+# Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
+resource "azurerm_resource_group" "rg-aks" {
+  name     = var.resource_group_name
   location = var.location
-}
-
-resource "azurerm_storage_account" "sta" {
-  name                     = var.sname
-  resource_group_name      = var.rgname
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "dev"
-  }
 }
