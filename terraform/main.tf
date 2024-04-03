16ea7d31093a61b7b8f7d1d3b6b8a819cd6e18f7
rattrap4u <ratrap4u@gmail.com>
2024-04-03 11:30:48 +0530

    terraform config file fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,12 @@ terraform {
 
 provider "azurerm" {
   features {}
-  skip_provider_registration = "true"
+  skip_provider_registration = true  # Skip automatic provider registration
+
+  client_id         = var.client_id
+  client_secret     = var.client_secret
+  subscription_id   = var.subscription_id
+  tenant_id         = var.tenant_id
 }
 
 resource "azurerm_resource_group" "rg" {
