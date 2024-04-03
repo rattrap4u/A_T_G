2df9c0908d21c622f4ee54220321ba911b2ed4c7
rattrap4u <ratrap4u@gmail.com>
2024-04-03 11:29:38 +0530

    terraform config file fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,17 @@
 variable "rgname" {
-  description = "Resource Group Name"
-  default     = "rg"
+    description = "Resource Group Name"
+    default     = "rg"
+    type        = string
 }
 
 variable "location" {
-  description = "Azure location"
-  default     = "centralindia"
+    description = "Azure location"
+    default     = "centralindia"
+    type        = string
 }
 
 variable "sname" {
-  description = "Azure Storage Account"
-  default     = "rgportstorage"
+    description = "Azure Storage Account"
+    default     = "rgportstorage"
+    type        = string
 }
