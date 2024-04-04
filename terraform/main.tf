07cc8a7f74482990a5ce8acaa7da8ddaaa23fcac
rattrap4u <ratrap4u@gmail.com>
2024-04-04 13:37:00 +0530

    Remove files from previous commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,19 +13,42 @@ provider "azurerm" {
 
 }
 
+# Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = var.rgname
-  location = var.location
+  name     = "Tushar"
+  location = "centralindia"
 }
 
-resource "azurerm_storage_account" "rg" {
-  name                     = "ptsta"
-  resource_group_name      = "rg"
-  location                 = "centralindia"
+# Create a storage account
+resource "azurerm_storage_account" "storage" {
+  name                     = "tusharport" 
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "dev"
+  account_replication_type  = "LRS"
+  account_kind              = "StorageV2"
+  
+  static_website {
+    index_document =  "index.html"
   }
 }
+
+# resource "azurerm_storage_container" "container" {
+#   name                  = "content"
+#   storage_account_name  = azurerm_storage_account.storage.name
+#   container_access_type = "private"
+# }
+
+# Add a index.html file
+resource "azurerm_storage_blob" "blob" {
+  name                 = "index.html"
+  storage_account_name = azurerm_storage_account.storage.name
+  storage_container_name = "$web"
+  type                 = "Block"
+  content_type         = "text/html"
+  source_content       = "<div><h2>Hello World</h2><a href=\"https://precious-llama-151610.netlify.app/\">Click here to visit my Portfolio</a></div>"
+}
+
+
+
+
